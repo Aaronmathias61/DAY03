b1273bb7b7e885f566d67894351ce4e6ffbd2860
Aaron Mathias <94035223+Aaronmathias61@users.noreply.github.com>
2021-11-28 21:43:53 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,16 @@
+var  resume={
+    "Name":"Aaron",
+    "Age":"24",
+    "DOB":"16.10.1997",
+    "Address":"[S/O Mathias,17 Aaron cottage, Alangulam-627851]",
+    "State":"Tamil Nadu",
+    "Nationality":"Indian",
+    "Educational Qualification":{"B.Tech":{"Institite":"Mepco Schlenk Engineering College","Year of passing":"2015-2019","Percentage":"73"},
+    "12th":{"Institite":"Bharath Montessori Metriculation Higher Sceondary School","Year of passing":"2014-2015","Percentage":"80"},
+    "10th":{"Institite":"St.Joseph Matri Hr. Sec. School","Year of passing":"2012-2013","Percentage":"91"}},
+    "Software Skills":["C","C++","Python","Javascript"],
+    "Skills Set":["Positivity","Adaptability","Probelm Sloving","Time Managemant"],
+    "Hobby":["Hockey Pleayer","Cricketer","Good Athelete","Football","Reading Bible"],
+    "Declaration":"I hereby declre that all the above mentioned particulars are true to the vest of my knowledge."
+}
+console.log(resume);

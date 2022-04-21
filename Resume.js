fffaa97c9145a4199e75904a92a444105ee4b228
Aaron Mathias <94035223+Aaronmathias61@users.noreply.github.com>
2022-04-21 14:47:44 +0530

    Update Resume.js

--- Resume.js
+++ Resume.js
@@ -8,7 +8,7 @@ var  resume={
     "Educational Qualification":{"B.Tech":{"Institite":"Mepco Schlenk Engineering College","Year of passing":"2015-2019","Percentage":"73"},
     "12th":{"Institite":"Bharath Montessori Metriculation Higher Sceondary School","Year of passing":"2014-2015","Percentage":"80"},
     "10th":{"Institite":"St.Joseph Matri Hr. Sec. School","Year of passing":"2012-2013","Percentage":"91"}},
-    "Software Skills":["C","C++","Python","Javascript"],
+    "Software Skills":["C","C++","Python","Javascript","HTMl","CSS","Bootstrap"],
     "Skills Set":["Positivity","Adaptability","Probelm Sloving","Time Managemant"],
     "Hobby":["Hockey Pleayer","Cricketer","Good Athelete","Football","Reading Bible"],
     "Declaration":"I hereby declre that all the above mentioned particulars are true to the vest of my knowledge."
